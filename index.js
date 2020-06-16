a58179296111e42e8f6d315ba140fb101d2b5626
Martijn de Munnik <martijndemunnik@protonmail.com>
2020-06-16 21:11:56 +0200

    Extracted transmission groups from code and optimize search with talkerId

--- index.js
+++ index.js
@@ -6,88 +6,7 @@ const pkgData = require('./package.json')
 
 // https://rietman.wordpress.com/2016/09/16/the-light-weight-ethernet-lwe-implementation-in-nemastudio/
 // https://furunousa.com/-/media/sites/furuno/document_library/documents/manuals/public_manuals/far3000_settings_and_adjustments_manual.pdf
-const TRANSMISSIONGROUPS = {
-  MISC: {
-    TALKERS: ['BI', 'DU', 'ER', 'II', 'NL', 'RC', 'SG', 'SS', 'UP', 'U0', 'U1', 'U2', 'U3', 'U4', 'U5', 'U6', 'U7', 'U8', 'U9', 'VR', 'YX', 'SI'],
-    ADDRESS: '239.192.0.1',
-    PORT: 60001
-  },
-  TGTD: {
-    TALKERS: ['AI', 'RA'],
-    ADDRESS: '239.192.0.2',
-    PORT: 60002
-  },
-  SATD: {
-    TALKERS: ['HE', 'HN', 'TI'],
-    ADDRESS: '239.192.0.3',
-    PORT: 60003
-  },
-  NAVD: {
-    TALKERS: ['AG', 'AP', 'DF', 'EC', 'EI', 'GA', 'GP', 'GL', 'GN', 'HC', 'HF', 'IN', 'LC', 'SD', 'SN', 'VD', 'VM', 'VW', 'WI'],
-    ADDRESS: '239.192.0.4',
-    PORT: 60004
-  },
-  VDRD: {
-    TALKERS: ['BN', 'FD', 'FE', 'FR', 'FS', 'HD', 'HS', 'WD', 'WL'],
-    ADDRESS: '239.192.0.5',
-    PORT: 60005
-  },
-  RCOM: {
-    TALKERS: ['CD', 'CR', 'CS', 'CT', 'CV', 'CX', 'EP'],
-    ADDRESS: '239.192.0.6',
-    PORT: 60006
-  },
-  TIME: {
-    TALKERS: ['ZA', 'ZC', 'ZQ', 'ZV'],
-    ADDRESS: '239.192.0.7',
-    PORT: 60007
-  },
-  PROP: {
-    TALKERS: [],
-    ADDRESS: '239.192.0.8',
-    PORT: 60008
-  },
-  USR1: {
-    TALKERS: [],
-    ADDRESS: '239.192.0.9',
-    PORT: 60009
-  },
-  USR2: {
-    TALKERS: [],
-    ADDRESS: '239.192.0.10',
-    PORT: 60010
-  },
-  USR3: {
-    TALKERS: [],
-    ADDRESS: '239.192.0.11',
-    PORT: 60011
-  },
-  USR4: {
-    TALKERS: [],
-    ADDRESS: '239.192.0.12',
-    PORT: 60012
-  },
-  USR5: {
-    TALKERS: [],
-    ADDRESS: '239.192.0.13',
-    PORT: 60013
-  },
-  USR6: {
-    TALKERS: [],
-    ADDRESS: '239.192.0.14',
-    PORT: 60014
-  },
-  USR7: {
-    TALKERS: [],
-    ADDRESS: '239.192.0.15',
-    PORT: 60015
-  },
-  USR8: {
-    TALKERS: [],
-    ADDRESS: '239.192.0.16',
-    PORT: 60016
-  },
-}
+
 const MULTICASTPREFIX = 'UdPbC\0'
 const MAXLINECOUNT = 1000
 
@@ -98,24 +17,26 @@ module.exports = function (app) {
   return {
     start: options => {
       app.debug(options)
+      let talkers = loadTalkers()
       if (options.ipaddress) {
         socket = dgram.createSocket('udp4')
         socket.bind(function(){
           socket.setMulticastInterface(options.ipaddress)
         })
-
         createTagBlock.lineCount = 0
+
         const send = originalMessage => {
-          transmissionGroup = findTransmissionGroup(originalMessage)
+          let talkerId = originalMessage.substring(1, 3)
           let newMessage = ''
-          if (transmissionGroup) {
+          if (talkerId in talkers) {
             newMessage += createPrefix(options)
             newMessage += createTagBlock(options)
             newMessage += originalMessage + '\r\n'
-            app.debug(JSON.stringify(`Multicasting ${newMessage} to ${transmissionGroup.ADDRESS}:${transmissionGroup.PORT}`))
-            socket.send(newMessage, 0, newMessage.length, transmissionGroup.PORT, transmissionGroup.ADDRESS)
+            app.debug(JSON.stringify(`Multicasting ${newMessage} to ${talkers[talkerId].address}:${talkers[talkerId].port}`))
+            socket.send(newMessage, 0, newMessage.length, talkers[talkerId].port, talkers[talkerId].address)
           }
         }
+
         if (typeof options.nmea0183 === 'undefined' || options.nmea0183) {
           app.signalk.on('nmea0183', send)
           onStop.push(() => {
@@ -128,7 +49,7 @@ module.exports = function (app) {
             app.removeListener('nmea0183out', send)
           })
         }
-        app.setProviderStatus(`Using interface with address ${address}`)
+        app.setProviderStatus(`Using interface with address ${options.ipaddress}`)
       } else {
         app.setProviderError('No address specified')
       }
@@ -194,9 +115,22 @@ function schema () {
   }
 }
 
-function findTransmissionGroup(originalMessage) {
-  let talkerId = originalMessage.substring(1, 3)
-  return Object.values(TRANSMISSIONGROUPS).find(group => group.TALKERS.includes(talkerId))
+function loadTalkers() {
+  let fs = require("fs")
+  let path = require('path')
+  let talkers = {}
+  let transmissionGroups = JSON.parse(fs.readFileSync(path.join(__dirname, 'transmissiongroups.json')))
+  for (let key in transmissionGroups) {
+    transmissionGroups[key].talkers.forEach(
+      talkerId => {
+        talkers[talkerId] = {
+          address: transmissionGroups[key].address,
+          port: transmissionGroups[key].port
+        }
+      }
+    )
+  }
+  return talkers
 }
 
 function createPrefix(options) {
